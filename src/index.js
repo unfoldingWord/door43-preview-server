84b50b328770013485760c705dd2f1bd21c2e709
Richard Mahn <rich.mahn@unfoldingword.org>
2024-05-23 09:21:03 -0400

    Adds try catch

--- src/index.js
+++ src/index.js
@@ -21,19 +21,23 @@ app.use(booleanParser());
 app.use(numberParser());
 
 async function print({ browser, htmlContents, options }) {
-  const page = await browser.newPage();
-  console.log("HERE", htmlContents, options);
-  await page.setContent(htmlContents, { waitUntil: 'networkidle0', timeout: 1800000 });
-  console.log("Generating PDF...");
-  const pdf = page.pdf(options);
-  console.log("Done.");
-  return pdf;
+  try {
+    const page = await browser.newPage();
+    console.log('HERE', htmlContents, options);
+    await page.setContent(htmlContents, { waitUntil: 'networkidle0', timeout: 1800000 });
+    console.log('Generating PDF...');
+    const pdf = await page.pdf(options);
+    console.log('Done.');
+    return pdf;
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+  }
 }
 
 function parseRequest(request) {
   return {
-    filename: (request.query.filename || 'document').replace(/(\.pdf)?$/, '.pdf'),                    // add .pdf extension if necessary
-    options: { format: 'a4', landscape: false, printBackground: true, ...request.query, path: null }  // discard potential `path` parameter
+    filename: (request.query.filename || 'document').replace(/(\.pdf)?$/, '.pdf'), // add .pdf extension if necessary
+    options: { format: 'a4', landscape: false, printBackground: true, ...request.query, path: null }, // discard potential `path` parameter
   };
 }
 
@@ -41,11 +45,15 @@ function convertToMs(str) {
   const units = str.slice(-1);
   const value = Number(str.slice(0, -1));
 
-  switch(units) {
-    case 's': return value * 1000;
-    case 'm': return value * 1000 * 60;
-    case 'h': return value * 1000 * 60 * 60;
-    default: throw new Error('Invalid time unit');
+  switch (units) {
+    case 's':
+      return value * 1000;
+    case 'm':
+      return value * 1000 * 60;
+    case 'h':
+      return value * 1000 * 60 * 60;
+    default:
+      throw new Error('Invalid time unit');
   }
 }
 
@@ -54,35 +62,45 @@ export function use(puppeteer) {
     return puppeteer.launch({
       executablePath: '/usr/bin/chromium-browser',
       headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
   }
 
   app.post('/', cors(), async (request, response) => {
-    const browser = await launchBrowser();
-    const { filename, options } = parseRequest(request);
-    const res = await print({ htmlContents: request.body, browser, options });
-    await browser.close();
-    response.attachment(filename.replace(/(?:\.pdf)?$/, '.pdf')).send(res);
+    try {
+      const browser = await launchBrowser();
+      const { filename, options } = parseRequest(request);
+      const res = await print({ htmlContents: request.body, browser, options });
+      await browser.close();
+      response.attachment(filename.replace(/(?:\.pdf)?$/, '.pdf')).send(res);
+    } catch (error) {
+      console.log('Error running the browser: ', error);
+    }
   });
 
   app.post('/multiple', cors(), async (request, response) => {
-    const browser = await launchBrowser();
-    const { filename, options } = parseRequest(request);
-    const files = await Promise.all(request.body.map(htmlContents => {
-      const { name: path, removeCallback: rm } = tmp.fileSync();
-      return print({ htmlContents, browser, options: { ...options, path: path } }).then(() => ({ path, rm }));
-    }));
+    try {
+      const browser = await launchBrowser();
+      const { filename, options } = parseRequest(request);
+      const files = await Promise.all(
+        request.body.map((htmlContents) => {
+          const { name: path, removeCallback: rm } = tmp.fileSync();
+          return print({ htmlContents, browser, options: { ...options, path: path } }).then(() => ({ path, rm }));
+        })
+      );
 
-    const res = files.reduce((merged, { path, rm }) => {
-      merged.addPagesOf(new pdf.ExternalDocument(fs.readFileSync(path)));
-      rm();
-      return merged;
-    }, new pdf.Document());
+      const res = files.reduce((merged, { path, rm }) => {
+        merged.addPagesOf(new pdf.ExternalDocument(fs.readFileSync(path)));
+        rm();
+        return merged;
+      }, new pdf.Document());
 
-    await browser.close();
-    const buffer = await res.asBuffer();
-    response.attachment(filename).send(buffer);
+      await browser.close();
+      const buffer = await res.asBuffer();
+      response.attachment(filename).send(buffer);
+    } catch (error) {
+      console.error('Error running multiple pages:', error);
+    }
   });
 
   app.options('/*', cors());
